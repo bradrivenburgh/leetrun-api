cda6fe03af48b6990d18c2a6dcd5e5fd62fa4a8b
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-13 21:46:19 -0500

    changed env name for DB to match Heroku standard

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000, 
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://user_name@localhost/database_name', // DB_URL fallback goes here
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://user_name@localhost/database_name', // DATABASE_URL fallback goes here
 }
