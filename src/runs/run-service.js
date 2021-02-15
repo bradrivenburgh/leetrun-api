daff27a548083260ca546a9ac2616d3df8ec9a45
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-15 14:44:05 -0500

    created some fixtures to test run-router

--- src/runs/run-service.js
+++ src/runs/run-service.js
@@ -7,26 +7,25 @@ const RunService = {
       .from('run_entries')
       .where('user_id', userId)
       .orderBy('date', 'desc');
+  },
+  serializeRuns(runEntries) {
+    const entry = runEntries;
+    return {
+      id: xss(entry.id),
+      date: xss(entry.date),
+      location: xss(entry.location),
+      distance: xss(entry.distance),
+      hours: xss(entry.hours),
+      minutes: xss(entry.minutes),
+      seconds: xss(entry.seconds),
+      notes: xss(entry.notes),
+      public: xss(entry.public),
+      weather: xss(entry.weather),
+      surface: xss(entry.surface),
+      terrain: xss(entry.terrain),
+      user_id: xss(entry.user_id),
+    }
   }
 };
-
-serializeRuns(runEntries) {
-  const entry = runEntries;
-  return {
-    id: xss(entry.id),
-    date: xss(entry.date),
-    location: xss(entry.location),
-    distance: xss(entry.distance),
-    hours: xss(entry.hours),
-    minutes: xss(entry.minutes),
-    seconds: xss(entry.seconds),
-    notes: xss(entry.notes),
-    public: xss(entry.public),
-    weather: xss(entry.weather),
-    surface: xss(entry.surface),
-    terrain: xss(entry.terrain),
-    user_id: xss(entry.user_id),
-  }
-}
 
 module.exports = RunService;
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,237 +1,196 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 function makeUsersArray() {
   return [
     {
       id: 1,
-      user_name: 'test-user-1',
-      first_name: 'Test 1',
-      last_name: 'User 1',
-      password: 'password',
-      date_created: new Date('2029-01-22T16:28:32.615Z'),
+      user_name: "test-user-1",
+      first_name: "Test 1",
+      last_name: "User 1",
+      password: "password",
+      date_created: new Date("2029-01-22T16:28:32.615Z"),
     },
     {
       id: 2,
-      user_name: 'test-user-2',
-      first_name: 'Test 2',
-      last_name: 'User 2',
-      password: 'password',
-      date_created: new Date('2029-01-22T16:28:32.615Z'),
+      user_name: "test-user-2",
+      first_name: "Test 2",
+      last_name: "User 2",
+      password: "password",
+      date_created: new Date("2029-01-22T16:28:32.615Z"),
     },
     {
       id: 3,
-      user_name: 'test-user-3',
-      first_name: 'Test 3',
-      last_name: 'User 3',
-      password: 'password',
-      date_created: new Date('2029-01-22T16:28:32.615Z'),
+      user_name: "test-user-3",
+      first_name: "Test 3",
+      last_name: "User 3",
+      password: "password",
+      date_created: new Date("2029-01-22T16:28:32.615Z"),
     },
     {
       id: 4,
-      user_name: 'test-user-4',
-      first_name: 'Test 4',
-      last_name: 'User 4',
-      password: 'password',
-      date_created: new Date('2029-01-22T16:28:32.615Z'),
+      user_name: "test-user-4",
+      first_name: "Test 4",
+      last_name: "User 4",
+      password: "password",
+      date_created: new Date("2029-01-22T16:28:32.615Z"),
     },
-  ]
+  ];
 }
 
-// function makeArticlesArray(users) {
-//   return [
-//     {
-//       id: 1,
-//       title: 'First test post!',
-//       style: 'How-to',
-//       author_id: users[0].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
-//     },
-//     {
-//       id: 2,
-//       title: 'Second test post!',
-//       style: 'Interview',
-//       author_id: users[1].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
-//     },
-//     {
-//       id: 3,
-//       title: 'Third test post!',
-//       style: 'News',
-//       author_id: users[2].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
-//     },
-//     {
-//       id: 4,
-//       title: 'Fourth test post!',
-//       style: 'Listicle',
-//       author_id: users[3].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
-//     },
-//   ]
-// }
+function makeRunEntriesArray(users) {
+  return [
+    {
+      id: "1",
+      date: "2021-02-01",
+      location: "Philadelphia, PA",
+      distance: "5",
+      hours: "00",
+      minutes: "17",
+      seconds: "00",
+      notes: "Great run!",
+      public: TRUE,
+      weather: "clear",
+      surface: "pavement",
+      terrain: "flat",
+      user_id: users[0].id,
+    },
+    {
+      id: "2",
+      date: "2021-02-01",
+      location: "Philadelphia, PA",
+      distance: "5",
+      hours: "00",
+      minutes: "17",
+      seconds: "00",
+      notes: "Great run!",
+      public: TRUE,
+      weather: "clear",
+      surface: "pavement",
+      terrain: "flat",
+      user_id: users[1].id,
+    },
+    {
+      id: "3",
+      date: "2021-02-01",
+      location: "Philadelphia, PA",
+      distance: "5",
+      hours: "00",
+      minutes: "17",
+      seconds: "00",
+      notes: "Great run!",
+      public: TRUE,
+      weather: "clear",
+      surface: "pavement",
+      terrain: "flat",
+      user_id: users[2].id,
+    },
+    {
+      id: "4",
+      date: "2021-02-01",
+      location: "Philadelphia, PA",
+      distance: "5",
+      hours: "00",
+      minutes: "17",
+      seconds: "00",
+      notes: "Great run!",
+      public: TRUE,
+      weather: "clear",
+      surface: "pavement",
+      terrain: "flat",
+      user_id: users[3].id,
+    },
+  ];
+}
 
-// function makeCommentsArray(users, articles) {
-//   return [
-//     {
-//       id: 1,
-//       text: 'First test comment!',
-//       article_id: articles[0].id,
-//       user_id: users[0].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 2,
-//       text: 'Second test comment!',
-//       article_id: articles[0].id,
-//       user_id: users[1].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 3,
-//       text: 'Third test comment!',
-//       article_id: articles[0].id,
-//       user_id: users[2].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 4,
-//       text: 'Fourth test comment!',
-//       article_id: articles[0].id,
-//       user_id: users[3].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 5,
-//       text: 'Fifth test comment!',
-//       article_id: articles[articles.length - 1].id,
-//       user_id: users[0].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 6,
-//       text: 'Sixth test comment!',
-//       article_id: articles[articles.length - 1].id,
-//       user_id: users[2].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//     {
-//       id: 7,
-//       text: 'Seventh test comment!',
-//       article_id: articles[3].id,
-//       user_id: users[0].id,
-//       date_created: new Date('2029-01-22T16:28:32.615Z'),
-//     },
-//   ];
-// }
+function makeExpectedRunEntry(users, entry) {
+  const user = users.find((user) => user.id === entry.user_id);
 
-// function makeExpectedArticle(users, article, comments=[]) {
-//   const author = users
-//     .find(user => user.id === article.author_id)
+  return {
+    id: entry.id,
+    date: entry.date,
+    location: entry.location,
+    distance: entry.distance,
+    hours: entry.hours,
+    minutes: entry.minutes,
+    seconds: entry.seconds,
+    notes: entry.notes,
+    public: entry.public,
+    weather: entry.weather,
+    surface: entry.surface,
+    terrain: entry.terrain,
+    user_id: user.id,
+  };
+}
 
-//   const number_of_comments = comments
-//     .filter(comment => comment.article_id === article.id)
-//     .length
+function makeMaliciousRunEntry(user) {
+  const maliciousRunEntry = {
+    id: "911",
+    date: "2021-02-01",
+    location: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    distance: "5",
+    hours: "00",
+    minutes: "17",
+    seconds: "00",
+    notes: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    public: TRUE,
+    weather: "clear",
+    surface: "pavement",
+    terrain: "flat",
+    user_id: user.id,
+  }
 
-//   return {
-//     id: article.id,
-//     style: article.style,
-//     title: article.title,
-//     content: article.content,
-//     date_created: article.date_created.toISOString(),
-//     number_of_comments,
-//     author: {
-//       id: author.id,
-//       user_name: author.user_name,
-//       full_name: author.full_name,
-//       nickname: author.nickname,
-//       date_created: author.date_created.toISOString(),
-//       date_modified: author.date_modified || null,
-//     },
-//   }
-// }
-
-// function makeExpectedArticleComments(users, articleId, comments) {
-//   const expectedComments = comments
-//     .filter(comment => comment.article_id === articleId)
-
-//   return expectedComments.map(comment => {
-//     const commentUser = users.find(user => user.id === comment.user_id)
-//     return {
-//       id: comment.id,
-//       text: comment.text,
-//       date_created: comment.date_created.toISOString(),
-//       user: {
-//         id: commentUser.id,
-//         user_name: commentUser.user_name,
-//         full_name: commentUser.full_name,
-//         nickname: commentUser.nickname,
-//         date_created: commentUser.date_created.toISOString(),
-//         date_modified: commentUser.date_modified || null,
-//       }
-//     }
-//   })
-// }
-
-// function makeMaliciousArticle(user) {
-//   const maliciousArticle = {
-//     id: 911,
-//     style: 'How-to',
-//     date_created: new Date(),
-//     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-//     author_id: user.id,
-//     content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
-//   }
-//   const expectedArticle = {
-//     ...makeExpectedArticle([user], maliciousArticle),
-//     title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
-//     content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
-//   }
-//   return {
-//     maliciousArticle,
-//     expectedArticle,
-//   }
-// }
+  const expectedRunEntry = {
+    ...makeExpectedRunEntry([user], maliciousRunEntry),
+    location: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+    notes: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+  }
+  return {
+    maliciousRunEntry,
+    expectedRunEntry,
+  }
+}
 
 function makeFixtures() {
-  const testUsers = makeUsersArray()
-  return { testUsers }
+  const testUsers = makeUsersArray();
+  const testEntries = makeRunEntriesArray();
+  return { testUsers, testEntries };
 }
 
 function cleanTables(db) {
-  return db.transaction(trx =>
-    trx.raw(
-      `TRUNCATE
+  return db.transaction((trx) =>
+    trx
+      .raw(
+        `TRUNCATE
         run_entries,
         leetrun_users
       `
-    )
-    .then(() =>
-      Promise.all([
-        trx.raw(`ALTER SEQUENCE leetrun_users_id_seq minvalue 0 START WITH 1`),
-        trx.raw(`SELECT setval('leetrun_users_id_seq', 0)`),
-      ])
-    )
-  )
+      )
+      .then(() =>
+        Promise.all([
+          trx.raw(
+            `ALTER SEQUENCE leetrun_users_id_seq minvalue 0 START WITH 1`
+          ),
+          trx.raw(`SELECT setval('leetrun_users_id_seq', 0)`),
+        ])
+      )
+  );
 }
 
 function seedUsers(db, users) {
-  const preppedUsers = users.map(user => ({
+  const preppedUsers = users.map((user) => ({
     ...user,
-    password: bcrypt.hashSync(user.password, 1)
-  }))
-  return db.into('leetrun_users').insert(preppedUsers)
+    password: bcrypt.hashSync(user.password, 1),
+  }));
+  return db
+    .into("leetrun_users")
+    .insert(preppedUsers)
     .then(() =>
       // update the auto sequence to stay in sync
-      db.raw(
-        `SELECT setval('leetrun_users_id_seq', ?)`,
-        [users[users.length - 1].id],
-      )
-    )
+      db.raw(`SELECT setval('leetrun_users_id_seq', ?)`, [
+        users[users.length - 1].id,
+      ])
+    );
 }
 
 // function seedArticlesTables(db, users, articles, comments=[]) {
@@ -267,9 +226,9 @@ function seedUsers(db, users) {
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
   const token = jwt.sign({ user_id: user.id }, secret, {
     subject: user.user_name,
-    algorithm: 'HS256',
-  })
-  return `Bearer ${token}`
+    algorithm: "HS256",
+  });
+  return `Bearer ${token}`;
 }
 
 module.exports = {
@@ -279,4 +238,4 @@ module.exports = {
   cleanTables,
   makeAuthHeader,
   seedUsers,
-}
+};
