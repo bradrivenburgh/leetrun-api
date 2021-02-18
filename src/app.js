ec73b1145149792e5d532e206e3d12444fa96555
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-18 10:59:04 -0500

    added API_KEY for access to landing page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const { validateBearerToken } = require('./middleware/client-auth');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const runRouter = require('./runs/run-router');
@@ -15,19 +16,6 @@ const app = express();
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
-
-// Define validation function
-function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  // move to the next middleware
-  next();
-}
 
 // Define error handler
 function errorHandler(error, req, res, next) {
@@ -50,7 +38,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/runs', runRouter);
 app.use(errorHandler);
 
-app.get('/', (req, res) => {
+app.get('/', validateBearerToken,  (req, res) => {
   res.send('Welcome to the LeetRun API');
 });
 
